ee4ddb1abffa8531285c4449911e1ed0561c293a
marcelinosdo <marcelinotecnologia@outlook.com>
2020-07-14 17:47:34 -0300

    atualizaçoes em main

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,2 +1,20 @@
+import manager.DriverManager;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.MainPage;
+
 public class main {
+
+    WebDriver driver = DriverManager.getDriver();
+    WebDriverWait wait = DriverManager.getDriverWait();
+
+    @Test
+    public void testeAcessaraPagna(){
+        System.out.println("*** teste acesso a pagna***");
+        MainPage mainPage = new MainPage(driver);
+        mainPage.accessPage();
+
+        DriverManager.endSession();
+    }
 }
