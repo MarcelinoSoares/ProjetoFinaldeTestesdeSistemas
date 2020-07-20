cf9f073032300a731f4d117b1e78743a63c6f0ef
marcelinosdo <marcelinotecnologia@outlook.com>
2020-07-20 09:35:58 -0300

    atualizacoes em Stepdefinitions

--- src/test/java/stepDefinitions/passosCenariosMarcelino.java
+++ src/test/java/stepDefinitions/passosCenariosMarcelino.java
@@ -15,22 +15,35 @@ public class passosCenariosMarcelino {
 
     @Dado("que o usuário acessa a página de um produto")
     public void acessarAPáginaDeUmProduto() {
+     driver.get("https://www.dafiti.com.br/Tapete-Felpudo-Classic-Casa-Dona-Bege-100cm-x-150cm-5671829.html");
     }
 
     @Quando("clica em comprar")
     public void clicarEmBotaoComprar() {
+     WebElement Btncomprar = driver.findElement(By.cssSelector("#add-to-cart > button"));
+
+     Btncomprar.click();
     }
 
     @Então("o produto é adicionado na sacola")
-    public void adicionarNaSacola() {
+    public void IrNaSacola() {
+     WebElement irnasacola = driver.findElement(By.cssSelector(".popover-go-to-cart-content > a"));
+
+     irnasacola.click();
     }
 
     @Dado("que o usuário possui um produto na sacola")
     public void possuirUmProdutoNaSacola() {
+     WebElement irnasacola = driver.findElement(By.cssSelector(".popover-go-to-cart-content > a"));
+
+     irnasacola.click();
     }
 
     @Quando("clica em ‘Remover produto’")
     public void clicarEmBotaoRemoverProduto() {
+     WebElement Btnremover = driver.findElement(By.cssSelector(".cart-product-action > a"));
+
+     Btnremover.click();
     }
 
     @Então("o produto é removido da sacola")
@@ -39,10 +52,21 @@ public class passosCenariosMarcelino {
 
     @Dado("que o usuário deseja comprar um calçado para presente")
     public void DesejarComprarUmCalçadoParaPresente() {
+     driver.get("https://www.dafiti.com.br/Tenis-adidas-Originals-Superstar-Slip-on-W-Preto-5125927.html");
+     WebElement selecionatamanho = driver.findElement(By.cssSelector("#add-to-cart > div.selectbox-2.add-to-cart-form-sizes > input[type=hidden]"));
+     WebElement selecionanumero = driver.findElement(By.cssSelector("#add-to-cart > div.selectbox-2.add-to-cart-form-sizes.popover-click.is-open > ul > li:nth-child(6)"));
+     WebElement Btncomprar = driver.findElement(By.cssSelector("#add-to-cart > button"));
+
+     selecionatamanho.click();
+     selecionanumero.click();
+     Btncomprar.click();
     }
 
     @Quando("clica em Embalagem para presente")
     public void clicarEmEmbalagemParaPresente() {
+     WebElement embalagemparapresente = driver.findElement(By.cssSelector("#cart_product_gift_AD464SCF72QTB-58"));
+
+     embalagemparapresente.click();
     }
 
     @Então("o link muda o texto para ‘Com embalagem para presente’")
@@ -55,6 +79,7 @@ public class passosCenariosMarcelino {
 
     @Dado("que um usuário adiciona um produto na sacola")
     public void AdicionarUmProdutoNaSacola() {
+
     }
 
     @Quando("aumenta a quantidade para {int}")
